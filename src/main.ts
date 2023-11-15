774bb4657d0543aeed93e3c3e2eca01afff30166
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2023-11-15 16:56:13 -0300

    Cors bug fixed

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors'
-import { urlencoded, json } from 'express';
+import { urlencoded, json, Request } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.use(cors<Request>());
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(3001);
