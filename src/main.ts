ac7579a7bf842ee68b1702515cbb333a8a59262e
Vitor Gabriel <vitorgabrielsbo1460@gmail.com>
2023-11-15 20:22:06 -0300

    Update Cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors'
+import cors from 'cors';
 import { urlencoded, json, Request } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors<Request>());
+  app.use(cors());
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(3001);
